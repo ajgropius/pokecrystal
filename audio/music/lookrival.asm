3a2b7393feca06dcfd9f3e79c3e58ad1c0730aa7
ajgropius <59681146+ajgropius@users.noreply.github.com>
2020-06-15 17:33:57 -0700

    Updated lookrival.asm (6/15/20)
    Changed rival encounter theme to be Lyra’s encounter theme! Thanks Mmmmmm!

--- audio/music/lookrival.asm
+++ audio/music/lookrival.asm
@@ -1,350 +1,507 @@
-Music_LookRival:
-	channel_count 4
-	channel 1, Music_LookRival_Ch1
-	channel 2, Music_LookRival_Ch2
-	channel 3, Music_LookRival_Ch3
-	channel 4, Music_LookRival_Ch4
+Music_Lyra:
+	musicheader 4, 1, Music_Lyra_Ch1
+	musicheader 1, 2, Music_Lyra_Ch2
+	musicheader 1, 3, Music_Lyra_Ch3
+	musicheader 1, 4, Music_Lyra_Ch4
 
-Music_LookRival_Ch1:
-	tempo 112
-	volume 7, 7
-	duty_cycle 1
-	pitch_offset 1
-	vibrato 18, 1, 5
-	stereo_panning FALSE, TRUE
-	note_type 12, 11, 2
+Music_Lyra_Ch1:
+	tempo 184
+	dutycycle 1
+	notetype $c, $85
+	octave 2
+	note A#, 1
+	note A#, 2
+	octave 3
+	note G#, 2
+	octave 4
+	note C_, 2
+	note D#, 9
+Music_Lyra_Ch1_loop:
+	octave 3
+	note D#, 6
+	note G_, 1
+	octave 4
+	note D#, 1
+	octave 3
+	note D_, 2
+	note D_, 4
+	note D_, 2
+	note C_, 6
+	note G_, 1
+	note A#, 1
+	octave 2
+	note G_, 1
+	octave 3
+	note F_, 2
+	octave 2
+	note __, 1
+	octave 3
+	note F_, 4
+	octave 2
+	notetype $3, $85
+	note G#, 1
+	octave 3
+	note D#, 15
+	notetype $c, $85
+	octave 2
+	note G#, 2
+	octave 3
+	note D#, 1
+	octave 4
+	note C_, 1
+	octave 2
+	note G_, 1
+	octave 3
+	note D#, 3
+	note D#, 2
+	octave 2
+	note G_, 2
+	note F_, 6
+	octave 3
+	note D#, 1
+	octave 2
+	note F_, 1
+	note A#, 1
+	octave 3
+	note F_, 2
+	octave 2
+	note A#, 1
+	note A#, 4
+	intensity $b4
+	note __, 2
+	octave 4
+	note G_, 1
+	note __, 1
+	note G_, 1
+	note G#, 1
+	note __, 1
+	note A#, 3
+	octave 5
+	note D#, 1
+	note __, 1
+	note D_, 1
+	note __, 1
+	octave 4
+	note A#, 1
+	note __, 1
+	note G_, 3
+	note __, 1
+	note G_, 1
+	note G#, 1
+	note __, 1
+	note G_, 5
+	note F_, 2
+	note G_, 2
+	note G#, 4
+	note A#, 1
+	octave 5
+	note C_, 1
+	note __, 1
+	octave 4
+	note A#, 5
+	note G_, 4
+	note G#, 2
+	note G_, 1
+	note __, 1
+	note G#, 1
+	note A#, 1
+	note __, 1
+	notetype $6, $b4
+	note G_, 8
+	note G#, 1
+	note G_, 1
+	note F_, 8
+	notetype $c, $86
+	note D#, 5
+	note C_, 1
+	note D_, 1
+	note D#, 1
+	note __, 1
+	note D_, 1
+	note __, 2
+	octave 3
+	note A#, 4
+	octave 4
+	note C_, 5
+	octave 3
+	note G#, 1
+	note A#, 1
+	octave 4
+	note C_, 1
+	note __, 1
+	octave 3
+	note A#, 1
+	note __, 2
+	note G_, 2
+	note A#, 2
+	intensity $84
+	note A_, 1
+	note F_, 1
+	note A_, 1
 	octave 4
 	note C_, 1
 	octave 3
-	note A#, 1
-	note G#, 2
-	note A#, 1
-	note G#, 1
-	note F#, 2
-	note F_, 6
-	volume_envelope 11, 7
-	note G#, 6
-	volume_envelope 11, 2
-	note A#, 4
-	note F_, 2
-	note G#, 4
-	volume_envelope 10, 0
-	note G#, 10
-	volume_envelope 10, 5
-	note G#, 8
-	volume_envelope 10, 6
-	note F#, 8
-.mainloop:
-	volume_envelope 11, 5
-	note F_, 4
-	note D#, 2
-	note F_, 6
-	note F_, 2
-	note G_, 2
-	note G#, 4
-	note A#, 4
-	note G#, 2
-	note G_, 4
-	volume_envelope 10, 0
-	note F_, 2
-	volume_envelope 10, 7
-	note F_, 16
-	volume_envelope 6, 6
-	rest 2
-	note F_, 2
-	note G_, 4
-	volume_envelope 11, 6
-	note G#, 4
+	note F_, 1
+	note A_, 1
 	octave 4
-	note C_, 4
+	note C_, 1
 	octave 3
-	note A#, 4
-	note G#, 2
-	note A#, 6
-	volume_envelope 11, 4
-	note A#, 2
+	note A_, 1
 	octave 4
-	note C_, 2
-	note C#, 2
-	note C#, 2
-	note D#, 2
-	note D#, 2
-	note C#, 2
-	note C_, 6
-	volume_envelope 11, 7
+	note C_, 1
+	note F_, 1
 	octave 3
-	note A#, 12
-	note G#, 2
-	note G_, 2
-	note G#, 2
-	note A#, 10
-	note B_, 4
+	note A_, 1
 	octave 4
-	note C_, 6
+	note C_, 1
+	note F_, 1
 	octave 3
-	note A#, 6
+	note A_, 1
 	octave 4
-	note D_, 2
-	note C_, 2
-	octave 3
-	note A#, 6
-	note G_, 10
-	note A#, 6
-	note G#, 6
-	octave 4
-	note C_, 2
-	octave 3
-	note A#, 2
-	note G#, 8
-	note F_, 6
-	note C_, 2
-.loop1:
-	volume_envelope 11, 2
-	note F_, 6
-	volume_envelope 11, 7
-	note G#, 4
-	note F#, 2
-	note F_, 2
-	note D#, 2
-	sound_loop 3, .loop1
-	volume_envelope 11, 2
-	note F_, 2
-	note G#, 4
-	volume_envelope 11, 7
-	note G#, 6
-	note F#, 4
-	sound_loop 0, .mainloop
-
-Music_LookRival_Ch2:
-	duty_cycle 3
-	vibrato 8, 3, 6
-	stereo_panning TRUE, FALSE
-	note_type 12, 12, 2
-	octave 4
-	note F#, 2
-	note F_, 2
-	note D#, 2
-	note C#, 2
-	note C_, 6
-	volume_envelope 12, 7
-	note D#, 6
-	volume_envelope 12, 2
-	note F_, 4
-	note C_, 2
-	note D#, 4
-	volume_envelope 11, 0
-	note D#, 10
-	volume_envelope 11, 7
-	note D#, 16
-.mainloop:
-	volume_envelope 12, 7
-	note C_, 12
-	volume_envelope 12, 4
-	note C_, 2
-	note D_, 2
-	note D#, 4
-	note D_, 4
-	note C_, 2
-	note D_, 4
-	volume_envelope 11, 0
-	note C_, 6
-	volume_envelope 11, 7
-	note C_, 8
-	volume_envelope 11, 4
-	octave 3
-	note A_, 2
-	note G_, 2
-	note A_, 2
-	volume_envelope 12, 6
-	octave 4
-	note C_, 6
-	volume_envelope 12, 7
-	note D#, 8
-	note F_, 12
-	volume_envelope 12, 4
-	note F_, 2
-	note G_, 2
-	note G#, 2
-	note G#, 2
-	note G_, 2
-	note G_, 2
-	note F_, 2
-	volume_envelope 12, 4
-	note G_, 6
-	volume_envelope 11, 0
-	note F_, 16
-	volume_envelope 11, 7
-	note F_, 16
-	volume_envelope 11, 0
-	note G_, 12
-	note F_, 4
-	note E_, 16
-	note F_, 12
-	note D#, 4
-	volume_envelope 12, 7
-	note D_, 8
-	volume_envelope 12, 4
-	note C_, 2
-	note D_, 2
+	note C_, 1
+	note F_, 1
 	note D#, 1
+	octave 3
+	note G#, 1
+	octave 4
+	note C_, 1
+	note D#, 1
+	octave 3
+	note G#, 1
+	octave 4
+	note C_, 1
+	note D#, 1
+	octave 3
+	note G#, 1
+	octave 4
+	note D_, 2
 	note D_, 1
 	note D#, 2
-.loop1:
-	volume_envelope 12, 2
-	note C_, 6
-	volume_envelope 12, 7
-	note D#, 6
-	volume_envelope 12, 2
-	note F_, 4
-	sound_loop 3, .loop1
-	note C_, 2
-	note D#, 4
-	volume_envelope 12, 7
-	note D#, 10
-	sound_loop 0, .mainloop
-
-Music_LookRival_Ch3:
-	note_type 12, 2, 8
-	rest 2
+	note F_, 3
+	loopchannel 0, Music_Lyra_Ch1_loop
+	
+Music_Lyra_Ch2:
+	dutycycle 2
+	notetype $c, $b4
 	octave 4
-	note C_, 2
-	octave 3
-	note G#, 2
-	note F#, 2
-	note F_, 4
-	note G#, 2
-	note D#, 4
-	note C_, 2
-	note D_, 2
-	note D#, 2
-	note G#, 2
-	note F#, 2
-	note F_, 2
-	note D#, 10
-	note D#, 10
-	note C_, 2
-	note D#, 2
-	note F#, 2
-.mainloop:
-.loop1:
-	sound_call .sub1
-	sound_loop 3, .loop1
-	note F_, 4
-	note C_, 2
-	note D#, 4
-	note G#, 2
-	note G_, 2
-	note F#, 2
-	sound_call .sub2
+	note C_, 1
+	note D#, 1
+	note __, 1
+	note G#, 1
+	note __, 1
+	octave 5
+	note C_, 1
+	note __, 1
+	intensity $b7
+	note D#, 9
+Music_Lyra_Ch2_loop:
+	note __, 4
+	octave 4
+	intensity $b4
+	note G_, 1
+	note G#, 1
+	note __, 1
+	intensity $b6
+	note A#, 5
+	intensity $b4
 	note A#, 2
-	note F_, 2
-	note G_, 2
-	note G#, 4
-	note F_, 2
+	octave 5
+	note C_, 2
+	octave 4
+	note D#, 4
+	note D#, 1
+	note F_, 1
+	note __, 1
+	intensity $b7
+	note G_, 9
+	note __, 2
+	intensity $b4
+	note C_, 2
+	note C_, 1
+	note D_, 2
+	note D#, 3
+	octave 3
+	note A#, 2
+	octave 4
+	note D#, 2
+	note A#, 2
+	note G#, 2
 	note G_, 2
 	note G#, 1
-	note A_, 1
-	sound_call .sub3
 	note A#, 2
-	note F_, 2
-	note G_, 2
-	note G#, 4
-	note F_, 2
-	note A#, 2
-	note B_, 2
-	octave 4
-	note C_, 4
+	note G_, 4
+	notetype $6, $B3
+	note G#, 1
+	note G_, 1
+	notetype $c, $B7
+	note F_, 4
+;;;;;;;;;;;mark
+	intensity $70
+	dutycycle 0
+	octave 2
+	note A#, 8
 	octave 3
-	note G_, 2
-	note A#, 4
-	note G_, 2
-	note A_, 2
-	note A#, 2
-	octave 4
-	note C_, 4
-	octave 3
-	note G_, 2
-	note A#, 4
-	note G_, 2
-	note A_, 2
-	note A#, 1
-	note A_, 1
-	sound_call .sub2
-	sound_call .sub3
-	sound_call .sub1
-	sound_call .sub1
-	note F_, 2
-	note C_, 2
-	note D_, 2
 	note D#, 4
-	note C_, 2
-	note D_, 2
+	note F_, 4
+	note G_, 6
+	notetype $8, $80
+	note F_, 1
+	note G_, 1
+	note F_, 1
+	notetype $c, $80
+	note E_, 8
+	note F_, 5
+	octave 2
+	note G#, 1
+	octave 3
+	note C_, 1
+	note F_, 1
+	note G_, 8
+	octave 2
+	note B_, 8
+	octave 3
+	note D#, 4
+	note F_, 4
+;;;;;;;;;;;;;;mark
+	intensity $b4
+	octave 4
+	dutycycle 2
+	note D#, 4
 	note D#, 1
-	note E_, 1
-	note F_, 2
-	note C_, 2
-	note D_, 2
-	note D#, 4
-	note C_, 2
-	note F#, 2
-	note E_, 2
-	sound_loop 0, .mainloop
-
-.sub1:
-	note F_, 2
-	note C_, 2
-	note D_, 2
-	note D#, 4
-	note C_, 2
-	note D_, 2
+	note G_, 2
+	note D_, 4
+	note D_, 1
+	note G_, 4
+	note C_, 4
+	note C_, 1
 	note D#, 2
-	sound_ret
-
-.sub2:
-	note A#, 2
-	note F_, 2
-	note G_, 2
-	note G#, 4
-	note F_, 2
-	note G_, 2
-	note G#, 2
-	sound_ret
-
-.sub3:
+	octave 3
 	note A#, 4
+	note A#, 1
+	octave 4
+	note D#, 2
+	note A#, 2
+	note A_, 6
+	note C_, 2
 	note F_, 2
-	note G#, 4
-	note F_, 2
-	note G_, 2
-	note G#, 2
-	sound_ret
+	note C_, 2
+	note G_, 1
+	note A_, 2
+	intensity $b7
+	note A#, 9
+	note __, 8
+	loopchannel 0, Music_Lyra_Ch2_loop
 
-Music_LookRival_Ch4:
-	stereo_panning TRUE, FALSE
-	toggle_noise 3
-	drum_speed 12
-	rest 8
-	rest 16
-	drum_note 4, 2
-	drum_note 3, 4
-	drum_note 3, 6
-	drum_note 3, 4
-	drum_note 4, 4
-	drum_note 4, 4
-	drum_note 3, 2
-	drum_note 3, 2
-	drum_note 3, 2
-	drum_note 3, 2
-.mainloop:
-	drum_note 4, 4
-	drum_note 3, 2
-	drum_note 4, 4
-	drum_note 4, 2
-	drum_note 3, 4
-	drum_note 4, 4
-	drum_note 3, 2
-	drum_note 4, 4
-	drum_note 4, 2
-	drum_note 3, 2
-	drum_note 3, 2
-	sound_loop 0, .mainloop
+Music_Lyra_Ch3:
+	notetype $c, $15
+	intensity $10
+	octave 2
+	note A#, 1
+	note A#, 1
+	note __, 1
+	note A#, 1
+	note __, 1
+	note A#, 1
+	note __, 1
+	note A#, 3
+	note __, 1
+	note A#, 3
+	note __, 1
+	note A#, 1
+Music_Lyra_Ch3_loop:
+	octave 3
+	note D#, 2
+	note __, 1
+	note D#, 1
+	octave 2
+	note A#, 2
+	note __, 1
+	note A#, 1
+	octave 3
+	note D_, 2
+	note __, 1
+	note D_, 1
+	octave 2
+	note A#, 2
+	note __, 1
+	note A#, 1
+	octave 3
+	note C_, 2
+	note __, 1
+	note C_, 1
+	note D#, 2
+	note __, 1
+	note D#, 1
+	octave 2
+	note G_, 2
+	note __, 1
+	note G_, 1
+	note G_, 2
+	note A#, 2
+	note G#, 2
+	note __, 1
+	note G#, 1
+	octave 3
+	note D#, 2
+	note __, 1
+	note D#, 1
+	octave 2
+	note G_, 2
+	note __, 1
+	note G_, 1
+	octave 3
+	note D#, 2
+	note __, 1
+	note D#, 1
+	note F_, 2
+	note __, 1
+	note F_, 1
+	note C_, 2
+	note __, 1
+	note C_, 1
+	octave 2
+	note A#, 2
+	note __, 1
+	note A#, 1
+	octave 3
+	note D_, 2
+	note F_, 2
+	note D#, 2
+	note __, 1
+	note D#, 1
+	octave 2
+	note A#, 2
+	note __, 1
+	note A#, 1
+	octave 3
+	note D_, 2
+	note __, 1
+	note D_, 1
+	octave 2
+	note A#, 2
+	note __, 1
+	note A#, 1
+	octave 3
+	note C#, 2
+	note __, 1
+	note C#, 1
+	octave 2
+	note G#, 2
+	note __, 1
+	note G#, 1
+	octave 3
+	note C_, 2
+	note __, 1
+	note C_, 1
+	note E_, 2
+	note G_, 2
+	note F_, 2
+	note __, 1
+	note F_, 1
+	note C_, 2
+	note __, 1
+	note C_, 1
+	note G_, 2
+	note __, 1
+	note G_, 1
+	note D#, 2
+	note __, 1
+	note D#, 1
+	note G#, 2
+	note __, 1
+	note G#, 1
+	note D#, 2
+	note __, 1
+	note D#, 1
+	note C#, 2
+	note __, 1
+	note C#, 1
+	octave 2
+	note A#, 2
+	octave 3
+	note F_, 2
+	note C_, 2
+	note __, 1
+	note C_, 1
+	note G_, 2
+	note __, 1
+	note G_, 1
+	octave 2
+	note G_, 2
+	note __, 1
+	note G_, 1
+	note A#, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note G#, 2
+	note __, 1
+	note G#, 1
+	octave 3
+	note D#, 2
+	note __, 1
+	note D#, 1
+	octave 2
+	note G_, 2
+	note __, 1
+	note G_, 1
+	note A#, 2
+	octave 3
+	note D#, 2
+	octave 2
+	note F_, 2
+	note __, 1
+	note F_, 1
+	octave 3
+	note C_, 2
+	note __, 1
+	note C_, 1
+	octave 2
+	note A_, 2
+	note __, 1
+	note A_, 1
+	octave 3
+	note F_, 2
+	note __, 1
+	note F_, 1
+	octave 2
+	note A#, 2
+	note __, 1
+	note A#, 1
+	octave 3
+	note F_, 2
+	note __, 1
+	note F_, 1
+	octave 2
+	note A#, 1
+	note __, 1
+	note A#, 1
+	octave 3
+	note C_, 2
+	note D_, 3
+	loopchannel 0, Music_Lyra_Ch3_loop
+	
+	
+Music_Lyra_Ch4:
+	togglenoise $3
+	notetype $6
+	note D_, 2
+	note D_, 4
+	note D_, 4
+	note D_, 4
+	note D_, 6
+	note D#, 1
+	note D#, 1
+	note D#, 1
+	note D#, 1
+	note C_, 2
+	note C_, 2
+	note D#, 4
+Music_Lyra_Ch4_loop:
+	note D#, 4
+	note D#, 2
+	note D#, 2
+	note C_, 4
+	note D#, 2
+	note D#, 2
+	loopchannel 0, Music_Lyra_Ch4_loop
